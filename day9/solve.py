7a51e4977dc8545a5a6edcf46ccc7b52ca48269a
Alex Telon <alex.telon@gmail.com>
2019-11-25 22:46:25 +0100

    day 9 part 2

--- day9/solve.py
+++ day9/solve.py
@@ -1,15 +1,19 @@
+from collections import defaultdict
+from pprint import pprint
+import itertools
 
-
+# Read data
 with open('input.txt') as f:
     data = f.read().splitlines()
 
+# remove fluff in input
 data = [x.split() for x in data]
+# from, to, distance
 data = [(x[0], x[2], int(x[4])) for x in data]
 
+# make sure to find all city names
 cities = set((x[0] for x in data))
 cities = cities.union(set((x[1] for x in data)))
-
-from collections import defaultdict
 
 city_distances = defaultdict(dict)
 for x in data:
@@ -23,16 +27,8 @@ for x in data:
     city_distances[(destination, origin)] = dist
 
 
-from pprint import pprint
-# pprint(city_distances)
-
-# We need to travel to each city. Minimize the total cost
-
-import itertools
 n = len(cities)
-print(f"nr of cities: {n}")
 permutations = itertools.permutations(cities, n)
-print(f"nr of permutations: {permutations}")
 
 def get_distance(*destinations):
     total = 0
@@ -42,17 +38,12 @@ def get_distance(*destinations):
     return total
 
 
-
 costs = {}
 for c in permutations:
     costs[c] = get_distance(*c)
 
-pprint(costs)
+print(f"nr of cities: {n}")
 print(f"nr of solutions: {len(costs)}")
 
-print(min(costs.values()))
-
-
-
-# breath first brute force
-# for city in city_distances:
+print(f"min: {min(costs.values())}")
+print(f"max: {max(costs.values())}")
