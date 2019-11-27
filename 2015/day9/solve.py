fc5e6360a4a1a09869d08a41dbc7c29e417cc86f
Alex Telon <alex.telon@gmail.com>
2019-11-27 17:51:12 +0100

    day9 added path namespace
    The __new__ function is not really needed, but it makes the
    "initialization" nicer.
    
    This was a bit tricky to get right since overriding __init__ in
    immutable classes is not what you expect. __new__ is a static method
    that is called before __init__ and is the right place to do stuff like
    this.

--- 2015/day9/solve.py
+++ 2015/day9/solve.py
@@ -1,5 +1,24 @@
 import itertools
 
+class path(frozenset):
+    """A path is represented by a frozenset.
+
+    The distance is symmetrical.
+    a -> b == b -> a
+
+    The representation of a path between two cities a and b should
+    therfore not take order into account. Hence a set.
+
+    Frozenset is a immutable set which means it can like tuple be
+    used as a key in a dictionary. (Remember dictionaries are hashmaps).
+    So the hash of a frozenset depends on the data, not the order.
+    """
+    def __new__(cls, *cities):
+        # This function allows us to initialize with the cities as a list of arguments.
+        # without this: path([a, b])
+        # with this:    path(a, b)
+        return frozenset.__new__(cls, cities)
+
 with open('input.txt') as f:
     data = f.read().splitlines()
 
@@ -14,13 +33,13 @@ cities = cities.union(set((x[1] for x in data)))
 
 city_distances = {}
 for origin, destination, dist in data:
-    city_distances[frozenset([origin, destination])] = dist
+    city_distances[path(origin, destination)] = dist
 
 def get_distance(*destinations):
     total = 0
     # loop over all destinations pairwise
     for start, stop in zip(destinations, destinations[1:]):
-        total += city_distances[frozenset([start, stop])]
+        total += city_distances[path(start, stop)]
 
     return total
 
