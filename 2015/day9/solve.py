3317773e88f945adf668303000f13ae5f4c8e1f1
Alex Telon <alex.telon@gmail.com>
2019-11-27 18:13:16 +0100

    day9 wrapped behaviour into functions

--- 2015/day9/solve.py
+++ 2015/day9/solve.py
@@ -1,5 +1,6 @@
 import itertools
 
+
 class path(frozenset):
     """A path is represented by a frozenset.
 
@@ -19,35 +20,54 @@ class path(frozenset):
         # with this:    path(a, b)
         return frozenset.__new__(cls, cities)
 
-with open('input.txt') as f:
-    data = f.read().splitlines()
 
-# remove fluff in input
-data = [x.split() for x in data]
-# from, to, distance
-data = [(x[0], x[2], int(x[4])) for x in data]
+def parse_data():
+    """Reads from input file and returns list of tuples.
 
-# make sure to find all city names
-cities = set((x[0] for x in data))
-cities = cities.union(set((x[1] for x in data)))
+    Example:
+        "Faerun to Tristram = 65" -> ("Faerun", "Tristram", 65)
+    """
+    with open('input.txt') as f:
+        data = [line.split() for line in f]
+    return [(x[0], x[2], int(x[4])) for x in data]
 
-city_distances = {}
-for origin, destination, dist in data:
-    city_distances[path(origin, destination)] = dist
+
+def get_cities(data):
+    """Gives a set of all cities mentioned in the data."""
+    cities = set((x[0] for x in data))
+    return cities.union(set((x[1] for x in data)))
+
+
+def city_distances(data):
+    """Creates a dictionary of where a path is the key and the value is the distance.
+
+    Example:
+        {
+            path("Faerun", "Tristram"): 65,
+            path("Tristram", "Tambi"): 63,
+        }
+    """
+    result = {}
+    for origin, destination, dist in data:
+        result[path(origin, destination)] = dist
+    return result
+
 
 def get_distance(*destinations):
     total = 0
     # loop over all destinations pairwise
     for start, stop in zip(destinations, destinations[1:]):
         total += city_distances[path(start, stop)]
-
     return total
 
+
+data = parse_data()
+cities = get_cities(data)
+city_distances = city_distances(data)
 costs = {}
 for c in itertools.permutations(cities):
     costs[c] = get_distance(*c)
 
 print(f"nr of cities: {len(cities)}, nr of solutions {len(costs)}")
-
 print(f"min: {min(costs.values())}")
 print(f"max: {max(costs.values())}")
