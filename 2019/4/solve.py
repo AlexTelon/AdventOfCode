f96541320f9cd6518d5e01a277a37a6b476f7183
Alex Telon <alex.telon@gmail.com>
2019-12-04 07:10:10 +0100

    2019 day4

--- 2019/4/solve.py
+++ 2019/4/solve.py
@@ -1,12 +1,38 @@
-import computer
+import itertools
 
-computer = computer.Computer(debug=True)
+def is_ok(num):
+    nums = [int(x) for x in str(num)]
+    found_eq = False
+    for prev, current in zip(nums, nums[1:]):
+        if current < prev:
+            return False
 
-computer.load_memory('input.txt')
+        # part 1
+        # if prev == current:
+        #     found_eq = True
 
-# computer.data = list(map(int, f.read().split(',')))
+        # part 2
+        groups = [list(g) for k, g in itertools.groupby(nums)]
+        if any(g for g in groups if len(g) == 2):
+            found_eq = True
 
-# print("-----------")
-# print(computer.data)
+    return found_eq
 
-computer.run()
+low, high = 109165, 576723
+
+# part 1
+# assert is_ok(111111) is True
+# assert is_ok(111123) is True
+# assert is_ok(135679) is False
+# assert is_ok(223450) is False
+# assert is_ok(123789) is False
+#part 2
+assert is_ok(111123) is False
+assert is_ok(111111) is False
+assert is_ok(112233) is True
+assert is_ok(123444) is False
+assert is_ok(111122) is True
+
+#
+# unique_passwords = set(password for password in range(low, high+1) if is_ok(password))
+print(len([ password for password in range(low, high+1) if is_ok(password) ]))
