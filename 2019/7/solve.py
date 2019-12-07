e7ed6cb28806b7fffcc82003ddf8fe77ffb31224
Alex Telon <alex.telon@gmail.com>
2019-12-07 13:18:03 +0100

    2019 day7 cleanup

--- 2019/7/solve.py
+++ 2019/7/solve.py
@@ -6,8 +6,8 @@ import queue
 from threading import Thread
 from collections import deque
 
+# Does not work anymore after I changed things for part2.
 # def test_combination_part_1(phase_combination):
-#     # phase = deque([1,0,4,3,2])
 #     phase = deque(phase_combination)
 #     prev_output = 0
 #     for i in range(5):
@@ -19,44 +19,37 @@ from collections import deque
 #         computer.input_queue.append(prev_output)
 
 #         computer.run()
-
 #         prev_output = computer.output
-#         # print("prev_output: ", prev_output)
 
 #     return prev_output
 
-counter = 0
+debug_counter = 0
 
 def test_combination(phase_combination):
-    global counter
-    counter += 1
+    global debug_counter
+    debug_counter += 1
+
     computers = []
     phase = deque(phase_combination)
-    prev_output = 0
 
-    # init all
+    # Initiaize the computers.
     for i in range(5):
         computer = Computer(debug=False)
         computer.load_memory('input.txt')
         computer.output_queue = queue.Queue()
-        computer.name = f"{i} run: {counter}"
+        computer.name = f"{i} run: {debug_counter}"
 
         computers.append(computer)
 
-    # for prev, current in zip(computers, computer[1:]):
-    #     current.input_queue =
-    computers[0].input_queue = computers[4].output_queue
-    computers[1].input_queue = computers[0].output_queue
-    computers[2].input_queue = computers[1].output_queue
-    computers[3].input_queue = computers[2].output_queue
-    computers[4].input_queue = computers[3].output_queue
-
-    d = computers[0].input_queue.qsize()
-    assert d == 0
+    # TODO async await instead?
+    # TODO generators instead?
 
     for i in range(5):
+        computers[i].input_queue = computers[i - 1].output_queue
         computers[i].input_queue.put(phase.popleft())
 
+    # Once all computers have gotten their phase nr first they all shall receive input form the previous one.
+    # However we must initialize the first one with 0.
     computers[0].input_queue.put(0)
 
     threads = []
@@ -71,13 +64,6 @@ def test_combination(phase_combination):
 
     return computers[-1].output
 
-# part1
-# print(max(test_combination_part_1(p) for p in permutations([4,3,2,1,0])))
+# print("part1: ", max(test_combination_part_1(p) for p in permutations([4,3,2,1,0])))
 
-# phases = [9,8,7,6,5]
-# print(test_combination(phases))
-
-# phases = [9,7,8,5,6]
-# print("last: ", test_combination(phases))
-# part2
-print(max(test_combination(p) for p in permutations([5,6,7,8,9])))
+print("part2: ", max(test_combination(p) for p in permutations([5,6,7,8,9])))
