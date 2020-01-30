82de39f5f9d18866760b41ce2f8dc8a6ef07dfe7
Alex Telon <alex.telon@gmail.com>
2020-01-30 11:18:55 +0100

    Removed the need to state how many params an instruction has
    Now you only need to tag a method as a instruction and it will make sure
    to fill in the parameters you chose correctly.

--- 2019/intcode/computer.py
+++ 2019/intcode/computer.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from collections import defaultdict
+from inspect import signature
 
 import collections
 import itertools
@@ -10,34 +11,26 @@ from defaultlist import defaultlist
 
 DebugInfo = namedtuple('DebugInfo', ['pc', 'name', 'args',])
 
-def instruction(nr_of_args):
+def instruction():
     def real_decorator(function):
         @functools.wraps(function)
         def wrapper(*args, **kwargs):
+
+            sig = signature(function)
+            # The number of parameters minus "self"
+            nr_of_args = len(sig.parameters) - 1
+
             self = args[0]
             instruction_args = self.fetch_and_step(nr_of_args)
 
+            # Gather some debug information
             info = DebugInfo(name=function.__name__,
                 pc=self.pc,
                 args=instruction_args
             )
             self.debug_info.append(info)
-            if self.debug:
-                print(f"{self.pc}{function.__name__: >10} {instruction_args}\t\t unpacked args: {[self.data[x] for x in instruction_args]}", end="")
 
-            # execute
-            # try:
             result = function(self, *instruction_args)
-            # except IndexError as e:
-            #     print()
-            #     print(f"args causing the index error: {instruction_args}")
-            #     print()
-            #     raise e
-
-            if self.debug:
-                pass
-                print(f"\t\t data at last arg position afterwards: {self.data[instruction_args[-1]]}", end="")
-                print(f"data[0]: {self.data[0]}")
             return result
         return wrapper
     return real_decorator
@@ -86,25 +79,20 @@ class Computer():
         else:
             return self.args_raw[index]
 
-    @instruction(3)
+    @instruction()
     def add(self, a, b, addr):
         addr = self.get_addr(2)
         self.data[addr] = a + b
 
-    @instruction(3)
+    @instruction()
     def multiply(self, a, b, _):
         addr = self.get_addr(2)
         self.data[addr] = a * b
 
-    # @instruction(3)
-    # def divide(self, a, b, c):
-    #     self.data[c] = (self.data[a] // self.data[b])
-
     def halt(self):
         self.abort = True
-        # print(f'computer-{self.name} HALT last output {self.output}')
 
-    @instruction(1)
+    @instruction()
     def input(self, _):
         addr = self.get_addr(0)
         item = int(input())
@@ -112,25 +100,23 @@ class Computer():
 
         self.data[addr] = item
 
-    @instruction(1)
+    @instruction()
     def output(self, a):
-        # print(f"computer-{self.name} output: {a}")
         print(f"output: {a}")
         self.output_stuff = a
-        # print(self.output)
         # self.output_queue.put(a)
 
-    @instruction(2)
+    @instruction()
     def jmp_true(self, expr, jmp_addr):
         if expr != 0:
             self.pc = jmp_addr
 
-    @instruction(2)
+    @instruction()
     def jmp_false(self, expr, jmp_addr):
         if expr == 0:
             self.pc = jmp_addr
 
-    @instruction(3)
+    @instruction()
     def less_than(self, a, b, _):
         addr = self.get_addr(2)
         result = 0
@@ -139,7 +125,7 @@ class Computer():
         self.data[addr] = result
 
 
-    @instruction(3)
+    @instruction()
     def equals(self, a, b, _):
         addr = self.get_addr(2)
         result = 0
@@ -147,40 +133,19 @@ class Computer():
             result = 1
         self.data[addr] = result
 
-    @instruction(1)
+    @instruction()
     def relative(self, shift):
         self.relative_base += shift
 
-    # @instruction(1)
-    # def pop(self, a):
-    #     self.data[a] = self.stack.popleft()
 
-    # @instruction(1)
-    # def push(self, a):
-    #     self.stack.pushleft(self.data[a])
-
-
-    # @property
-    # def pc(self):
-    #     return self.__pc
-
-    # @pc.setter
-    # def pc(self, value):
-    #     self.__pc = value
-
-
-    def __init__(self, debug=False):
+    def __init__(self):
         self.pc = 0
         self.abort = False
-        self.debug = debug
 
         self.debug_info = []
 
         self.relative_base = 0
 
-        # 3: self.minus,
-        # 4: self.divide,
-        # 21: self.jmp,
         self.op_codes = {
             1: self.add,
             2: self.multiply,
@@ -195,8 +160,6 @@ class Computer():
         }
 
     def _exec_instruction(self, op):
-        # if self.debug:
-        #     print(f"{self.op_codes[op].__name__}")
         instruction = self.op_codes[op]
         instruction()
 
@@ -229,21 +192,9 @@ class Computer():
         while not self.abort:
             self.tick()
 
+
 if __name__ == "__main__":
 
-    computer = Computer(debug=False)
-
+    computer = Computer()
     computer.load_memory('input.txt')
-
-    # computer.data = list(map(int, f.read().split(',')))
-
-    # print("-----------")
-    # print(computer.data)
-
     computer.run()
-
-    # print("-----------")
-    # print(computer.data)
-
-# TODO add a way to show where PC is relative to data
-# TODO add a way to convert all OP_codes in data to named functions and then split after the right nr of args
