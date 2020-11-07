e602418e093c1938d22e3c9a2f5a5b3369b1e36f
Alex Telon <alex.telon@gmail.com>
2020-11-07 19:04:30 +0100

    day6 part 1

---
+++ 2015/6/solve.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+lines = open('input.txt').read().splitlines()
+
+# Grid where everything is off (0) by default.
+grid = defaultdict(int)
+
+def action(a,b, func):
+    global grid
+    x_range = range(a[0], b[0]+1)
+    y_range = range(a[1], b[1]+1)
+    for x in x_range:
+        for y in y_range:
+            grid[(x,y)] = func(grid[(x,y)])
+
+data = []
+
+actions = {
+    'turn_on':  lambda x: 1,
+    'turn_off': lambda x: 0,
+    'toggle': lambda x: 1 if x == 0 else 0,
+}
+
+for i, line in enumerate(lines):
+    op, a, _, b = line.split()
+    a = tuple(int(x) for x in a.split(','))
+    b = tuple(int(x) for x in b.split(','))
+    print(i, len(lines))
+    action(a, b, actions[op])
+
+print(sum(grid.values()))
