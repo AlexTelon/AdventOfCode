c6fbae2b660aead709986e18352b2c9cca1074ca
Alex Telon <alex.telon@gmail.com>
2020-12-08 06:35:53 +0100

    2020 day8 part2

--- 2020/8/solve.py
+++ 2020/8/solve.py
@@ -1,61 +1,61 @@
-from collections import defaultdict, Counter
-import itertools
-from itertools import product, permutations, combinations, repeat
-import queue
-from threading import Thread
-from collections import deque
-from decimal import Decimal
-from fractions import Fraction
-import fractions
-import networkx
-import string
-import operator
-## read file line by line
-# some code that reads a file line by line
 lines = open('input.txt').read().splitlines()
 instructions = [line.split() for line in lines]
 
 accumulator = 0
 pointer = 0
 visited = set()
+done = False
 
 def acc(inc):
     global accumulator
     accumulator += inc
 
 def jmp(rel):
-    global pointer
+    global pointer, done
     pointer += rel
+    if pointer == len(instructions):
+        print('part2', accumulator)
+        exit()
     if pointer not in visited:
         visited.add(pointer)
     else:
-        print(f'visited {pointer} a second time!')
-        print('part1', accumulator)
-        exit()
+        # print(f'will visit {pointer} a second time!')
+        # print('part1', accumulator)
+        # exit()
+        done = True
 
 def nop(num):
     pass
 
 op_codes = {
-    'acc': acc,
+    'acc' : acc,
     'jmp' : jmp,
     'nop' : nop,
 }
 
+def run_computer(instructions):
+    while not done:
+        op, num = instructions[pointer]
+        num = int(num)
+        # print(pointer, op, num)
+        op_codes[op](num)
+        if op != 'jmp':
+            op_codes['jmp'](1)
 
+for i, (op, num) in enumerate(instructions):
+    # Only swap nop and jmp's.
+    if op == "acc":
+        continue
+    new_op = 'jmp' if op == 'nop' else 'nop'
+    print(f'outer loop {i}, {new_op} changed')
 
-# for op, num in instructions:
-while True:
-    op, num = instructions[pointer]
-    num = int(num)
-    print(pointer, op, num)
-    op_codes[op](num)
-    if op != 'jmp':
-        op_codes['jmp'](1)
+    # Change instruction set to one with the changed op-code.
+    tmp_instructions = instructions.copy()
+    tmp_instructions[i] = (new_op, num)
 
-    
-    # print(accumulator, pointer)
-# jmp relative +2 skips next instruction
-# jmp relative -1 go to 1 above it
-
-#1589
+    # Reset global state
+    accumulator = 0
+    pointer = 0
+    visited = set()
+    done = False
+    run_computer(tmp_instructions)
