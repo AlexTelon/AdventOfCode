cf17dcb719033f5c637ac121f3cbdfb094783b87
Alex Telon <alex.telon@gmail.com>
2020-12-12 07:16:58 +0100

    2020 day12

--- 2020/12/solve.py
+++ 2020/12/solve.py
@@ -1,93 +1,51 @@
-from collections import defaultdict, Counter
-import itertools
-from itertools import product, permutations, combinations, repeat
-import queue
-from threading import Thread
-from collections import deque
-from decimal import Decimal
-from fractions import Fraction
-import fractions
-import networkx
-import string
-import operator
-
-# fractions tips
-## Directions
-# directions = {
-#     '^': (0, 1),
-#     'v': (0, -1),
-#     '<': (-1, 0),
-#     '>': (1, 0),
-# }
-# dirs = [directions[c] for line in open('input.txt') for c in line]
-
-## Alphabet
-# alphas = string.ascii_lowercase 
-
-# op_codes = {
-#     'AND': operator.and_,
-#     'OR' : operator.or_,
-#     'RSHIFT' : operator.rshift,
-#     'NOT' : lambda a,b: not b
-# }
-
-# product('ABCD', repeat=2)                 -> AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
-# permutations('ABCD', 2)                   -> AB AC AD BA BC BD CA CB CD DA DB DC
-# combinations('ABCD', 2)                   -> AB AC AD BC BD CD
-# combinations_with_replacement('ABCD', 2)  -> AA AB AC AD BB BC BD CC CD DD
-
-#data = [tuple(map(int, line.strip().split('x'))) for line in open('input.txt')]
-
-# if you have multiple lines of stuff in groups, and groups seperated by empty lines.
-# groups = open(0).read().split("\n\n")
-# for group in groups:
-#     for line in group.splitlines():
-#         print(line)
-
-## read file line by line
-# some code that reads a file line by line
 lines = open('input.txt').read().splitlines()
-# lines = [ for line in lines]
 
+def rotate(direction, vector):
+    """Takes a direction N or L and rotates a vector accordingly around origo"""
+    # Swap and Negate
+    # Draw the point (2, 1) on a coodrinate axis and rotate 4 times.
+    # Note how x,y is always swaped. And depending on if you rotate left or right you negate one of the numbers.
+    if direction == 'L':
+        vector[0], vector[1] = vector[1], -vector[0]
+    else:
+        vector[0], vector[1] = -vector[1], vector[0]
+    return vector
 
 directions = {
-    'N':    (1, 0),
-    'E':    (0, 1),
-    'S':    (-1, 0),
-    'W':    (0, -1),
-    'L':    (0, 0, -1),
-    'R':    (0, 0, 1),
-    'F':    (0, 0, 0),
+    'N': (1, 0),
+    'E': (0, 1),
+    'S': (-1, 0),
+    'W': (0, -1),
 }
+
 dirs = "NESW"
 dir = 'E'
 pos = [0, 0]
-angle = 90
+forward_vector = [1, 10]
 
 for line in lines:
     c = line[0]
-    vector = directions[c]
     num = int(line[1:])
+
     if c in 'NESW':
-        pos[0] += vector[0] * num
-        pos[1] += vector[1] * num
+        vector = directions[c]
+        forward_vector[0] += vector[0] * num
+        forward_vector[1] += vector[1] * num
+        # print(f"new forward_vector {forward_vector}")
+
     if c in 'LR':
         assert num in [0, 90, 180, 270]
         num = [0, 90, 180, 270].index(num)
-        if c == 'L':
-            num = -num
-        new_index = (dirs.index(dir) + num) % 4
-        old_dir = dir
-        dir = dirs[new_index]
-        # dir = (dir + vector[2]) % 4
-        # print(f'added {num} new angle', angle)
-        print(f'{old_dir} -> {dir}', 'num,', num)
-    if c == 'F':
-        facing_vector = directions[dir]
-        print("facing: {dir}", facing_vector)
-        pos[0] += facing_vector[0] * num
-        pos[1] += facing_vector[1] * num
-    print(pos)
 
+        for i in range(num):
+            rotate(c, forward_vector)
+        # print(f"new forward_vector {forward_vector} rotated {c} {num} times")
+
+    if c == 'F':
+        # print(f"forward_vector: {forward_vector} {num} times")
+        pos[0] += forward_vector[0] * num
+        pos[1] += forward_vector[1] * num
+    # print(pos)
+
+print(f'final pos {pos}')
 print(sum(abs(p) for p in pos))
-# not 330
