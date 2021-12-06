7265ac70782b157fed3e8d88e36ec021e5961eb7
Alex Telon <alex.telon@gmail.com>
2021-12-06 07:36:37 +0100

    2021 day6 nicer solution
    Heavily inspired by r/adventofcode.

--- 2021/6/nice.py
+++ 2021/6/nice.py
@@ -1,11 +1,21 @@
-from collections import Counter
+from collections import deque
 
-count = Counter(map(int, open('input.txt').read().split(',')))
+# Inspired by:
+# 1. https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnfaisu/?utm_source=share&utm_medium=web2x&context=3
+# 2. https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnfd2w5/?utm_source=share&utm_medium=web2x&context=3
 
-for _ in range(256): # p1 use 80
-    count = Counter({k-1:v for k,v in count.items()})
-    count[8] += count[-1]
-    count[6] += count[-1]
-    del count[-1]
+# (1) was first but (2) was more concise.
+# (2) does a rotation on lists manually. So here is a solution using deques with maxsize where we can use the builtin rotate for deques.
 
-print(sum(count.values()))
+# rotate(-1) is not self evident so added a lambda to make it clearer.
+
+data = open('input.txt').read()
+fish = deque([data.count(i) for i in '012345678'], maxlen=9)
+
+rotate_left = lambda f: f.rotate(-1)
+
+for _ in range(256):
+    rotate_left(fish)
+    fish[6] += fish[-1]
+
+print(sum(fish))
