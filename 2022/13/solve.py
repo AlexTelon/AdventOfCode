1918ea95c9c733ee754759bd47e70b132625188d
Alex Telon <alex.telon@gmail.com>
2022-12-13 10:34:55 +0100

    2022 day 13 - 35 lines

--- 2022/13/solve.py
+++ 2022/13/solve.py
@@ -3,25 +3,21 @@ from functools import cmp_to_key
 def larger_than(A: str, B: str):
     A, B = eval(A), eval(B)
     for a,b in zip(A, B):
-        match a, b:
-            case [int(), int()]:
-                if   a < b: return -1
-                elif a > b: return 1
-                else: continue
-            case [list(), int() as b]:
-                b = [b]
-            case [int() as a, list()]:
-                a = [a]
+        match [a, b]:
+            case [int() as a, int() as b] if a < b:  return -1
+            case [int() as a, int() as b] if a > b:  return 1
+            case [int() as a, int() as b] if a == b: continue
+            case [list(), int() as b]:    b = [b]
+            case [int() as a, list()]:    a = [a]
 
         res = larger_than(str(a),str(b))
         if res == 0: continue
         return res
 
-    if (type(A) == list) and (type(B) == list):
-        if   len(A) < len(B): return -1
-        elif len(A) > len(B): return 1
-        else:                 return 0
-    raise Exception('should not get here')
+    # Two lists that could not be distinguished by pairwise comparisons.
+    if   len(A) < len(B): return -1
+    elif len(A) > len(B): return 1
+    else:                 return 0
 
 pairs = open('input.txt').read().split('\n\n')
 lines = ['[[2]]', '[[6]]']
