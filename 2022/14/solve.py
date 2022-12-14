6a9388aef44372e056094de462394dc9dc73f46f
Alex Telon <alex.telon@gmail.com>
2022-12-14 07:08:27 +0100

    2022 day14

--- 2022/14/solve.py
+++ 2022/14/solve.py
@@ -1,6 +1,5 @@
 from itertools import pairwise
 import math
-from aocd import submit
 
 def touching(a, b):
     return math.dist((a.real,a.imag),(b.real,b.imag)) < 2
@@ -12,7 +11,6 @@ def move_towards(a, b):
     return a + complex(dx, dy)
 
 lines = open('in.txt').read().splitlines()
-# lines = open('sample.txt').read().splitlines()
 
 start = 500 + 0j
 
@@ -21,24 +19,26 @@ MIN_Y =  math.inf
 MAX_X = -math.inf
 MIN_X =  math.inf
 
-
-paths = []
-for line in lines:
-    points = [complex(*map(int,x.split(','))) for x in line.split('->')]
-    paths.append(points)
-
-# print(MAX_Y)
-# print(MIN_Y)
-# print(MAX_X)
-# print(MIN_X)
-# exit()
-
 def update_bounds(pos):
     global MAX_Y, MIN_Y, MAX_X, MIN_X
     MAX_Y = max(MAX_Y, int(pos.imag))
     MIN_Y = min(MIN_Y, int(pos.imag))
     MAX_X = max(MAX_X, int(pos.real))
     MIN_X = min(MIN_X, int(pos.real))
+
+
+paths = []
+for line in lines:
+    points = [complex(*map(int,x.split(','))) for x in line.split('->')]
+    paths.append(points)
+    for pos in points:
+        update_bounds(pos)
+
+paths.append([complex(MIN_X-1000,MAX_Y+2), complex(MAX_X+1000,MAX_Y+2)])
+
+update_bounds(paths[-1][0])
+update_bounds(paths[-1][1])
+update_bounds(start)
 
 grid = {}
 grid[start] = 'S'
@@ -51,18 +51,14 @@ for path in paths:
             current = move_towards(current, goal)
             grid[current] = '#'
 
-for pos in grid:
-    update_bounds(pos)
-
-
 def print_grid(viz):
     def pixel(x,y):
-        if (x,y) == (500, 0):
-            return 'S'
         if complex(x,y) in grid:
             return grid[complex(x,y)]
         if complex(x,y) in viz:
             return viz[complex(x,y)]
+        if (x,y) == (500, 0):
+            return 'S'
         return '.'
 
     for y in range(MIN_Y, MAX_Y+1):
@@ -84,10 +80,10 @@ def simulate_sand(pos):
         for delta in [down, down_left, down_right]:
             candidate = current + delta
 
-            air = candidate not in grid
+            air = candidate not in grid or candidate == start
             if air:
                 # move through air.
-                viz[candidate] = '~'
+                # viz[candidate] = '~'
                 current = candidate
                 stopped = False
                 break
@@ -95,16 +91,19 @@ def simulate_sand(pos):
                 continue
         if stopped:
             break
-        if current.imag > MAX_Y:
+        # # p1
+        # if current.imag > MAX_Y:
+        #     print(sum(v == 'o' for v in grid.values()))
+        #     quit()
+        # p2
+        if grid[500 + 0j] == 'o':
             print(sum(v == 'o' for v in grid.values()))
             quit()
-            break
     grid[current] = 'o'
     return viz
 
 while True:
-
-    viz = simulate_sand(start)
+    viz = simulate_sand(start-1j)
     # print_grid(viz)
     # if input() in ['q', 'quit']:
     #     quit()
