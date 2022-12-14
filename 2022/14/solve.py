ea6b7530ea9ea179ff52b791e139a709e7dae8e4
Alex Telon <alex.telon@gmail.com>
2022-12-14 07:22:22 +0100

    wip clean

--- 2022/14/solve.py
+++ 2022/14/solve.py
@@ -2,17 +2,20 @@ from itertools import pairwise
 import math
 
 def touching(a, b):
+    # Slightly modified copy from day 9.
     return math.dist((a.real,a.imag),(b.real,b.imag)) < 2
 
 def move_towards(a, b):
+    # Slightly modified copy from day 9.
     if not touching(a, b):
         dy = [1, -1][b.imag < a.imag] if b.imag != a.imag else 0
         dx = [1, -1][b.real < a.real] if b.real != a.real else 0
     return a + complex(dx, dy)
 
 lines = open('in.txt').read().splitlines()
+# lines = open('sample.txt').read().splitlines()
 
-start = 500 + 0j
+START = 500 + 0j
 
 MAX_Y = -math.inf
 MIN_Y =  math.inf
@@ -38,10 +41,10 @@ paths.append([complex(MIN_X-1000,MAX_Y+2), complex(MAX_X+1000,MAX_Y+2)])
 
 update_bounds(paths[-1][0])
 update_bounds(paths[-1][1])
-update_bounds(start)
+update_bounds(START)
 
 grid = {}
-grid[start] = 'S'
+grid[START] = 'S'
 for path in paths:
     for a, goal in pairwise(path):
         grid[a] = '#'
@@ -51,26 +54,18 @@ for path in paths:
             current = move_towards(current, goal)
             grid[current] = '#'
 
-def print_grid(viz):
-    def pixel(x,y):
-        if complex(x,y) in grid:
-            return grid[complex(x,y)]
-        if complex(x,y) in viz:
-            return viz[complex(x,y)]
-        if (x,y) == (500, 0):
-            return 'S'
+def print_grid():
+    def pixel(pos):
+        if pos in grid: return grid[pos]
         return '.'
 
     for y in range(MIN_Y, MAX_Y+1):
         for x in range(MIN_X, MAX_X+1):
-            print(end=pixel(x,y))
+            print(end=pixel(complex(x,y)))
         print()
 
 def simulate_sand(pos):
-    viz = {}
-    # goal is always down.
     down = 1j
-    # if blocked try these
     down_left  = -1 + 1j
     down_right = 1 + 1j
 
@@ -80,30 +75,19 @@ def simulate_sand(pos):
         for delta in [down, down_left, down_right]:
             candidate = current + delta
 
-            air = candidate not in grid or candidate == start
+            air = candidate not in grid or candidate == 500 + 0j
             if air:
-                # move through air.
-                # viz[candidate] = '~'
                 current = candidate
                 stopped = False
                 break
-            else:
-                continue
+            continue
         if stopped:
             break
-        # # p1
-        # if current.imag > MAX_Y:
-        #     print(sum(v == 'o' for v in grid.values()))
-        #     quit()
-        # p2
-        if grid[500 + 0j] == 'o':
-            print(sum(v == 'o' for v in grid.values()))
-            quit()
     grid[current] = 'o'
-    return viz
 
 while True:
-    viz = simulate_sand(start-1j)
-    # print_grid(viz)
-    # if input() in ['q', 'quit']:
-    #     quit()
+    simulate_sand(START-1j)
+
+    if grid[500 + 0j] == 'o':
+        print(sum(v == 'o' for v in grid.values()))
+        quit()
