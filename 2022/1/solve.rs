7fefcd7df2cfaf7534927b81c73e470a9e185338
Alex Telon <alex.telon@gmail.com>
2023-03-08 08:35:21 +0100

    Refactoring

--- 2022/1/solve.rs
+++ 2022/1/solve.rs
@@ -3,22 +3,15 @@ use std::io::{BufReader, Read};
 
 fn main() {
     let file = File::open("input.txt").unwrap();
-
     let mut reader = BufReader::new(file);
-
     let mut content = String::new();
     reader.read_to_string(&mut content).unwrap();
 
-    let chunks: Vec<&str> = content.split("\n\n").collect();
-
-    let mut sums: Vec<i32> = Vec::new();
-    for chunk in chunks {
-        sums.push(
-            chunk.split("\n")
-                .map(|x| x.parse::<i32>().unwrap())
-                .sum()
-        )
-    }
+    let mut sums: Vec<i32> = content.split("\n\n")
+                                    .map(|chunk| chunk.split("\n")
+                                                    .map(|x| x.parse::<i32>().unwrap())
+                                                    .sum())
+                                    .collect();
 
     println!("part1: {}", sums.iter().max().unwrap());
 
